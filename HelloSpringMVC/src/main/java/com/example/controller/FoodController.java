889f4ac2a59b08de5a3e0a897f8b1901882ec5f3
unknown <amilevich@yahoo.com>
2020-03-05 11:43:02 -0500

    spring mvc

--- HelloSpringMVC/src/main/java/com/example/controller/FoodController.java
+++ HelloSpringMVC/src/main/java/com/example/controller/FoodController.java
@@ -3,6 +3,10 @@ package com.example.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,5 +47,14 @@ public class FoodController {
 	public List<Food> getAllFood(){
 		return foodDao.selectAll();
 	}
-
+	//GetMapping, PutMapping
+	@PostMapping(value="/getFoodById.app", produces="application/json", params= {"id"})
+	public ResponseEntity<Food> getFoodById(int id){
+		return new ResponseEntity<Food>(foodDao.selectById(id), HttpStatus.OK);
+	}
+	
+	@PostMapping(value="{num}/getFoodByUri.app")
+	public Food getFoodByUri(@PathVariable("num") int num) {
+		return foodDao.selectById(num);
+	}
 }
